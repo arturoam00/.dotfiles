a5a45527f3c7c5a996c34b2283e24bf022e2b4bc
Arturo Abril <arturoabrilm@gmail.com>
2025-04-23 08:46:21 +0200

    nothing important yet

--- shell/.config/shell/bashrc.d/mamba.sh
+++ shell/.config/shell/bashrc.d/mamba.sh
@@ -1,7 +1,7 @@
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'micromamba shell init' !!
-# export MAMBA_EXE='/user/amartine/.local/bin/micromamba';
-# export MAMBA_ROOT_PREFIX='/data/atlas/users/amartine/.micromamba';
+# Both MAMBA_EXE and MAMBA_ROOT_PREFIX __need to be defined__ for this to work. 
+# This is better done in either `.env` or `.profile`, the first always takes precedence.
 __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
