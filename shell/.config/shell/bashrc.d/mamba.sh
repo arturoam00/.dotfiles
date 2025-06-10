cda7834b2d2af6cda917cfe2a29da718f9073b22
Arturo Abril Martinez <arturoabrilm@gmail.com>
2025-06-10 14:08:44 +0200

    changed lfrc

--- shell/.config/shell/bashrc.d/mamba.sh
+++ shell/.config/shell/bashrc.d/mamba.sh
@@ -1,7 +1,14 @@
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'micromamba shell init' !!
 # Both MAMBA_EXE and MAMBA_ROOT_PREFIX __need to be defined__ for this to work. 
 # This is better done in either `.env` or `.profile`, the first always takes precedence.
+
+if [ -z "$MAMBA_EXE" ] || [ -z "$MAMBA_ROOT_PREFIX" ]; then
+    echo "Error: Required environment variables MAMBA_EXE and/or MAMBA_ROOT_PREFIX are not set."
+    echo "Please define them in your .env or .profile file."
+    return 1 2>/dev/null || exit 1
+fi
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'micromamba shell init' !!
 __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
