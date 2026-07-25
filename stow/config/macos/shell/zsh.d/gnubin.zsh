277f5423ef48d65a5ade040ee298a3422dd14947
Arturo Abril <arturoabrilm@gmail.com>
2026-07-25 15:54:04 +0200

    updated macos packing stuff

--- stow/config/macos/shell/zsh.d/gnubin.zsh
+++ stow/config/macos/shell/zsh.d/gnubin.zsh
@@ -1,7 +1,12 @@
 typeset -U path PATH
 
 GNUBIN="/opt/homebrew/opt/coreutils/libexec/gnubin"
+GNUFIND="/opt/homebrew/opt/findutils/libexec/gnubin"
 
 if [[ -d $GNUBIN ]]; then
     export PATH="$GNUBIN:$PATH"
+fi
+
+if [[ -d $GNUFIND ]]; then
+    export PATH="$GNUFIND:$PATH"
 fi
