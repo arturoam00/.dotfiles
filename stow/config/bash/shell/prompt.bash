6b1b07134ba0ca0c3fb5d90b931349215cba5eb4
Arturo Abril <arturoabrilm@gmail.com>
2026-07-27 11:25:36 +0200

    updated remote env // updated prompt

--- stow/config/bash/shell/prompt.bash
+++ stow/config/bash/shell/prompt.bash
@@ -1,1 +1,4 @@
-PS1='\[\e[38;5;245m\]╭─\[\e[0m\] \[\e[38;5;109m\](${CONDA_DEFAULT_ENV:-${VIRTUAL_ENV_PROMPT:-base}})\[\e[0m\] \[\e[38;5;215m\]\w\[\e[0m\]$(__git_ps1 " \[\e[38;5;245m\]on\[\e[0m\] \[\e[38;5;142m\]󰊢 %s\[\e[0m\]")\n\[\e[38;5;245m\]╰─\[\e[0;38;5;215m\]❯\[\e[0m\] '
+PS1='\[\e[38;5;245m\]╭─\[\e[0m\] \[\e[38;5;109m\] \h\[\e[0m\] \[\e[38;5;215m\]\w\[\e[0m\]$(__git_ps1 " \[\e[38;5;245m\]on\[\e[0m\] \[\e[38;5;142m\]󰊢 %s\[\e[0m\]")\n\[\e[38;5;245m\]╰─\[\e[0;38;5;215m\]❯\[\e[0m\] '
+
+# Show the active conda/venv environment instead of the hostname:
+# PS1='\[\e[38;5;245m\]╭─\[\e[0m\] \[\e[38;5;109m\](${CONDA_DEFAULT_ENV:-${VIRTUAL_ENV_PROMPT:-base}})\[\e[0m\] \[\e[38;5;215m\]\w\[\e[0m\]$(__git_ps1 " \[\e[38;5;245m\]on\[\e[0m\] \[\e[38;5;142m\]󰊢 %s\[\e[0m\]")\n\[\e[38;5;245m\]╰─\[\e[0;38;5;215m\]❯\[\e[0m\] '
--- stow/config/zsh/shell/prompt.zsh
+++ stow/config/zsh/shell/prompt.zsh
@@ -12,5 +12,9 @@ zstyle ':vcs_info:git:*' actionformats ' %{%F{245}%}on%{%f%} %{%F{142}%}󰊢 %b%
 
 add-zsh-hook precmd vcs_info
 
-PROMPT='%{%F{245}%}╭─%{%f%} %{%F{109}%}(${CONDA_DEFAULT_ENV:-${VIRTUAL_ENV_PROMPT:-base}})%{%f%} %{%F{215}%}%~%{%f%}${vcs_info_msg_0_}
+PROMPT='%{%F{245}%}╭─%{%f%} %{%F{109}%} %m%{%f%} %{%F{215}%}%~%{%f%}${vcs_info_msg_0_}
 %{%F{245}%}╰─%{%f%}%(?.%{%F{215}%}.%{%F{196}%})❯%{%f%} '
+
+# Show the active conda/venv environment instead of the hostname:
+# PROMPT='%{%F{245}%}╭─%{%f%} %{%F{109}%}(${CONDA_DEFAULT_ENV:-${VIRTUAL_ENV_PROMPT:-base}})%{%f%} %{%F{215}%}%~%{%f%}${vcs_info_msg_0_}
+# %{%F{245}%}╰─%{%f%}%(?.%{%F{215}%}.%{%F{196}%})❯%{%f%} '
