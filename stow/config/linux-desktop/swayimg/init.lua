310f80f78b49aac7ac2ac459185532a018d1b99f
Arturo Abril <arturoabrilm@gmail.com>
2026-08-02 03:46:20 +0200

    added swayimg scripting

---
+++ stow/config/linux-desktop/swayimg/init.lua
@@ -0,0 +1,33 @@
+-- VIEWER MODE --
+
+local mwh_dump = os.getenv("HOME") .. "/.local/bin/mwh_dump"
+
+-- Exit with q
+swayimg.viewer.on_key("q", function()
+  swayimg.exit()
+end)
+
+-- Send image to the trash
+swayimg.viewer.on_key("Delete", function()
+  local image = swayimg.viewer.get_image()
+  if image then
+    os.execute(string.format('gio trash -- "%s"', image.path))
+    swayimg.text.set_status("File "..image.path.." removed")
+  end
+end)
+
+-- mwh script binding
+swayimg.viewer.on_key("Ctrl-p", function()
+    local image = swayimg.viewer.get_image()
+    if image then
+        os.execute(string.format('"%s" "%s"', mwh_dump, image.path))
+    end
+end)
+
+-- GALLERY MODE --
+
+-- Exit with q
+swayimg.gallery.on_key("q", function()
+  swayimg.exit()
+end)
+
